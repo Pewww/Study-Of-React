7c1574b50b0bca4ff45d6873883d1dea87dfc992
KoreanJungYJ <jung22337@naver.com>
2018-09-03 23:15:07 +0900

    인풋 이벤트 예제 추가

--- Study-Of-React/todo-list/src/components/EventPractice.js
+++ Study-Of-React/todo-list/src/components/EventPractice.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+  state = {
+    name: ''
+  }
+
+  inputEvent = ({ target }) => {
+    const { value } = target;
+
+    this.setState({
+      [ target.name ]: value
+    });
+  }
+
   render () {
     return (
       <div>
         <h1>App</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="이름을 입력해주세요."
+          value={ this.state.name }
+          onChange={ this.inputEvent }
+        />
+        <div>입력한 값: { this.state.name }</div>
       </div>
     );
   }
