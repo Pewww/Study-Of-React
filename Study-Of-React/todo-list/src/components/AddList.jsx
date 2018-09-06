3429d32e311b1d96afec9ddb6d1c85e18c3e165a
KoreanJungYJ <jung22337@naver.com>
2018-09-06 09:29:37 +0900

    리스트 삭제 기능 구현

--- Study-Of-React/todo-list/src/components/AddList.jsx
+++ Study-Of-React/todo-list/src/components/AddList.jsx
@@ -14,17 +14,38 @@ class AddList extends Component {
 
   addAnimalList = () => {
     const { lists, animal } = this.state;
-  
+    
+    if (animal !== '') {
+      this.setState({
+        lists: lists.concat(animal),
+        animal: ''
+      });
+    }
+  }
+
+  removeAnimal = (index) => {
+    const { lists } = this.state;
+    
+    // 전개 연산자를 이용하여 클릭한 index를 제외한 값들을 배열에 넣어주기
+    // splice로 배열을 직접 변경하는 것이 아닌, slice를 이용하여 새로운 배열을 만들어 내는 것
+
     this.setState({
-      lists: lists.concat(animal),
-      animal: ''
+      lists: [
+        ...lists.slice(0, index),
+        ...lists.slice(index + 1, lists.length)
+      ]
     });
   }
 
   render () {
     const { animal, lists } = this.state;
     const showLists = lists.map((list, index) => (
-      <li key={ index }>{ list }</li>
+      <li
+        key={ index }
+        onClick={ () => this.removeAnimal(index) }
+      >
+        { list }
+      </li>
     ));
 
     return (
@@ -38,6 +59,7 @@ class AddList extends Component {
           name="animal"
           value={ animal }
           onChange={ this.setAnimalName }
+          onKeyUp={ (e) => { e.key === 'Enter' ? this.addAnimalList() : '' } }
         />
         <button onClick={ this.addAnimalList }>추가</button>
       </div>
