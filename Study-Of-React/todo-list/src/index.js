0077611a5c479d84c05c899c42858819f8dd653d
KoreanJungYJ <jung22337@naver.com>
2018-09-11 15:59:43 +0900

    index.js 파일에서 ReactDOM.render을 destructuring을 이용하여 Render구문으로 변경

--- Study-Of-React/todo-list/src/index.js
+++ Study-Of-React/todo-list/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render as Render } from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 기존의 코드는 ReactDOM을 import한 후 ReactDOM.render로 했었음
+// import 구문에서 destructuring을 이용하여 Render로 변환
+Render(<App />, document.getElementById('root'));
 registerServiceWorker();
