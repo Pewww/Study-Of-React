966c9676ddb26005bac7237b30e284b2c74f4e48
KoreanJungYJ <jung22337@naver.com>
2018-10-10 22:57:31 +0900

    멀티 카운터 예제 등록

---
+++ Study-Of-React/multi-counter/src/components/Counter.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Counter.css';
+
+const Counter = ({ number, color, index, onIncrement, onDecrement, onSetColor }) => {
+  return (
+    <div
+      className="counter"
+      onClick={() => onIncrement(index)}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        onDecrement(index);
+      }}
+      onDoubleClick={() => onSetColor(index)}
+      style={{
+        backgroundColor: color
+      }}
+    >
+      { number }
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  index: PropTypes.number,
+  number: PropTypes.number,
+  color: PropTypes.string,
+  onIncrement: PropTypes.func,
+  onDecrement: PropTypes.func,
+  onSetColor: PropTypes.func
+};
+
+Counter.defaultProps = {
+  index: 0,
+  number: 0,
+  color: 'black',
+  onIncrement: () => console.warn('Function is not defined'),
+  onDecrement: () => console.warn('Function is not defined'),
+  onSetColor: () => console.warn('Function is not defined')
+};
+
+export default Counter;
